263dd90409a5806f58951c80702850af5156209a
Maksym Kozlov <54378060+SkiF-ex@users.noreply.github.com>
2020-09-28 20:31:43 +0300

    delete excess file

--- next.config.js
+++
@@ -1,8 +0,0 @@
-import { nextI18NextRewrites } from 'next-i18next/rewrites'
-
-const localeSubpaths = {}
-
-export async function rewrites() { return nextI18NextRewrites(localeSubpaths) }
-export const publicRuntimeConfig = {
-    localeSubpaths,
-}
